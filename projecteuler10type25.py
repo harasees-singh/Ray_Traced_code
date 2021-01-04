892219ad4a2c1273f0d446d297814cf3c595c691
Harasees Singh <76965050+harasees-singh@users.noreply.github.com>
2021-01-04 23:14:24 +0530

    Add files via upload

---
+++ projecteuler10type25.py
@@ -0,0 +1,39 @@
+# this code prints primes less than the number taken as input starting from 2
+import time
+import math
+x = int(input("enter num here :) "))
+
+t = time.time()
+
+
+def prime(num):
+    if num == 2:
+        return True
+    if num == 3:
+        return True
+    if num % 2 == 0:
+        return False
+
+    for i in range(3, math.floor(math.sqrt(num)) + 1, 2):
+        if num % i == 0:
+            return False
+    return True
+
+
+collection = [2, 3]
+
+for k in range(1, x//6 + 2):
+    if prime(6*k - 1) and (6*k - 1) < x:
+        if collection.count(6*k - 1) == 0:
+            collection.append(6*k - 1)
+    if prime(6*k + 1) and (6*k + 1) < x:
+        if collection.count(6 * k + 1) == 0:
+            collection.append(6*k + 1)
+s = 0
+for z in collection :
+    s += z
+
+
+print(collection)
+print(f"execution time = {time.time()- t}")
+print(f"sum of primes = {s}")
